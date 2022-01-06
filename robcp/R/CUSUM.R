b0d3e5e6aad0ac43075f0f7d216d0a099d8cd616
Sheila Goerz <sheila.goerz@tu-dortmund.de>
2022-01-06 11:50:34 +0100

    ...

--- robcp/R/CUSUM.R
+++ robcp/R/CUSUM.R
@@ -67,7 +67,7 @@ CUSUM <- function(x, method = "kernel", control = list(), inverse = "Cholesky", 
       x.adj[(k+1):n] <- x.adj[(k+1):n] - mean(x[(k+1):n]) + mean(x[1:k])
       rho <- cor(x.adj[-n], x.adj[-1], method = "spearman")
       
-      param <- max(ceiling(n^(1/3) * ((2 * rho) / (1 - rho^2))^(2/3)), 1)
+      param <- max(ceiling(n^(p1) * ((2 * rho) / (1 - rho^2))^(p2)), 1)
       param <- min(param, n-1)
 
       control$b_n <- param
