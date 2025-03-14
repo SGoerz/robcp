e75cbe2c38f24b0600789bb57c48aa4ae8f5b1d3
Sheila Görz <sheila.goerz@tu-dortmund.de>
2025-03-14 11:05:20 +0100

    change GMD, MD

--- src/CUSUM2.c
+++ src/CUSUM2.c
@@ -214,55 +214,55 @@ SEXP CUSUM_var(SEXP X, SEXP X2)
   return(RES);
 }
 
-
-SEXP MD(SEXP X, SEXP CUMMED, SEXP N)
-{
-  double n = *REAL(N);
-  double *x = REAL(X);
-  double *cummed = REAL(CUMMED);
-
-  SEXP RES;
-  PROTECT(RES = allocVector(REALSXP, n-1));
-  double *res = REAL(RES);
-
-  int i, k;
-
-  for(k = 1; k < n; k++)
-  {
-    res[k-1] = 0;
-    for(i = 0; i <= k; i++)
-    {
-      res[k-1] += fabs(x[i] - cummed[k]);
-    }
-  }
-
-  UNPROTECT(1);
-  return RES;
-}
-
-
-SEXP GMD(SEXP X, SEXP N)
-{
-  double n = *REAL(N);
-  double *x = REAL(X);
-
-  SEXP RES;
-  PROTECT(RES = allocVector(REALSXP, n-1));
-  double *res = REAL(RES);
-
-  int i, k;
-
-  res[0] = fabs(x[0] - x[1]);
-
-  for(k = 2; k < n; k++)
-  {
-    res[k-1] = res[k-2];
-    for(i = 0; i < k; i++)
-    {
-      res[k-1] += fabs(x[i] - x[k]);
-    }
-  }
-
-  UNPROTECT(1);
-  return RES;
-}
+// 
+// SEXP MD(SEXP X, SEXP CUMMED, SEXP N)
+// {
+//   double n = *REAL(N);
+//   double *x = REAL(X);
+//   double *cummed = REAL(CUMMED);
+// 
+//   SEXP RES;
+//   PROTECT(RES = allocVector(REALSXP, n-1));
+//   double *res = REAL(RES);
+// 
+//   int i, k;
+// 
+//   for(k = 1; k < n; k++)
+//   {
+//     res[k-1] = 0;
+//     for(i = 0; i <= k; i++)
+//     {
+//       res[k-1] += fabs(x[i] - cummed[k]);
+//     }
+//   }
+// 
+//   UNPROTECT(1);
+//   return RES;
+// }
+// 
+// 
+// SEXP GMD(SEXP X, SEXP N)
+// {
+//   double n = *REAL(N);
+//   double *x = REAL(X);
+// 
+//   SEXP RES;
+//   PROTECT(RES = allocVector(REALSXP, n-1));
+//   double *res = REAL(RES);
+// 
+//   int i, k;
+// 
+//   res[0] = fabs(x[0] - x[1]);
+// 
+//   for(k = 2; k < n; k++)
+//   {
+//     res[k-1] = res[k-2];
+//     for(i = 0; i < k; i++)
+//     {
+//       res[k-1] += fabs(x[i] - x[k]);
+//     }
+//   }
+// 
+//   UNPROTECT(1);
+//   return RES;
+// }
